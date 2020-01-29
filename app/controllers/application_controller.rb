880c19e2ba0bba7934b22c378a2997dfe96abf22
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-29 22:33:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
     if params[:username]=="" || params[:password]==""
       redirect to "/failure"
     else
-      user = User.find_by(username:params[:username])
+      user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect to "/account"
