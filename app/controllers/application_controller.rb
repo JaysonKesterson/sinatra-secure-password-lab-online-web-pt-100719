1a42e92f04230b2e2da118627320c54f4e166396
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-29 21:46:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,10 +19,10 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     @user = User.new(username: params[:username], password: params[:password])
     if @user.username == nil
-       redirect to '/failure'
+       redirect '/failure'
     end
-    else
-     
+    if user.save
+      
     end
 
   end
